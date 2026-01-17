57db0e7096b83dfb6a5978d64e6c16e66ae8c258
Drishan Vig <103531171+Drishanv@users.noreply.github.com>
2026-01-17 21:59:24 +0530

    Create db_connection.py

---
+++ database/db_connection.py
@@ -0,0 +1,23 @@
+import mysql.connector
+import pandas as pd
+import streamlit as st
+
+def get_connection():
+    try:
+        conn = mysql.connector.connect(
+            host=st.secrets["mysql"]["host"],
+            user=st.secrets["mysql"]["user"],
+            password=st.secrets["mysql"]["password"],
+            database=st.secrets["mysql"]["database"],
+            port=st.secrets["mysql"]["port"]
+        )
+        return conn
+    except Exception as e:
+        st.error("❌ Database connection failed")
+        st.stop()
+
+def run_query(query):
+    conn = get_connection()
+    df = pd.read_sql(query, conn)
+    conn.close()
+    return df
